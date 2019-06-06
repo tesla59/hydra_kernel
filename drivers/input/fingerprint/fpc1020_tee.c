16095d7b3303e252d14c404ba6fbbf58939dd1a7
Yaroslav Furman <yaro330@gmail.com>
2019-06-06 09:54:04 +0300

    fpc1020_ree: use device struct as wakeup source
    Also cleanup fingerprint model check while we are at it.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/input/fingerprint/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1020_tee.c
@@ -88,8 +88,6 @@ struct fpc1020_data {
 	struct pinctrl_state *pinctrl_state[ARRAY_SIZE(pctl_names)];
 	struct regulator *vreg[ARRAY_SIZE(vreg_conf)];
 
-	//struct wake_lock ttw_wl;
-	struct wakeup_source ttw_ws;//for kernel 4.9
 	int irq_gpio;
 	int rst_gpio;
 	struct mutex lock; /* To set/get exported values in sysfs */
@@ -574,7 +572,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
 		//wake_lock_timeout(&fpc1020->ttw_wl,
 		//			msecs_to_jiffies(FPC_TTW_HOLD_TIME));
-		__pm_wakeup_event(&fpc1020->ttw_ws, FPC_TTW_HOLD_TIME);//for kernel 4.9
+		pm_wakeup_event(fpc1020->dev, FPC_TTW_HOLD_TIME);//for kernel 4.9
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
@@ -698,10 +696,10 @@ static int fpc1020_probe(struct platform_device *pdev)
 		goto exit;
 	}
 
-	if(fpsensor != 1){
-                 pr_err("Macle fpc1020_probe failed as fpsensor=%d(1=fp)\n", fpsensor);
-                 return -1;
-         }
+	if (fpsensor != 1) {
+		pr_err("Macle fpc1020_probe failed as fpsensor=%d(1=fp)\n", fpsensor);
+		return -1;
+	}
 
 
 	fpc1020->dev = dev;
@@ -778,8 +776,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 	/* Request that the interrupt should be wakeable */
 	enable_irq_wake(gpio_to_irq(fpc1020->irq_gpio));
 
-	//wake_lock_init(&fpc1020->ttw_wl, WAKE_LOCK_SUSPEND, "fpc_ttw_wl");
-	wakeup_source_init(&fpc1020->ttw_ws, "fpc_ttw_ws");//for kernel 4.9
+	device_init_wakeup(dev, true);
 
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
@@ -820,8 +817,6 @@ static int fpc1020_remove(struct platform_device *pdev)
 	msm_drm_unregister_client(&fpc1020->fb_notifier);
 	sysfs_remove_group(&pdev->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1020->lock);
-	//wake_lock_destroy(&fpc1020->ttw_wl);
-	wakeup_source_trash(&fpc1020->ttw_ws);//for kernel 4.9
 	(void)vreg_setup(fpc1020, "vdd_ana", false);
 	(void)vreg_setup(fpc1020, "vdd_io", false);
 	(void)vreg_setup(fpc1020, "vcc_spi", false);
